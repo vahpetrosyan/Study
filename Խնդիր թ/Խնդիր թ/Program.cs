e2efd379d049085ad97e7992f8a5c01760e3cf5d
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-06 14:24:43 +0400

    Խնդիր 2

---
+++ Խնդիր թ/Խնդիր թ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Խնդիր_թ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            int K = 8;
+            for(int B = 1; B < 11; B++)
+            {
+                Console.WriteLine(K);
+            }
+            Console.ReadKey();
+        }
+    }
+}
