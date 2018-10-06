5e974039f452830244a9d36cae0a8cdcdafc68ff
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-06 14:56:07 +0400

    խնդիր 3

---
+++ Խնդիր 3/Խնդիր 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Խնդիր_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                if (i < a)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
