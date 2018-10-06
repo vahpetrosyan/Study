ba27269f9faa0e5a61ff45b6bc6da95a76a6d435
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-06 15:02:19 +0400

    խնդիր 4

---
+++ խնդիր 4/խնդիր 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i < 10; i++)
+            {
+            double result = Math.Pow(firstValue, i);
+            Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
