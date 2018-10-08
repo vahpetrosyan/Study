69170e195575f260cfdd7798258d4170b20eba38
vahpetrosyan <vah.petrosyan@gmail.com>
2018-10-08 18:36:39 +0400

    initial commit

---
+++ First_Stage/Types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = 19;
+            char symbol = a;
+            double d = 123;
+        }
+    }
+}
---
+++ խնդիր 9/խնդիր 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i < input; i++)
+            {
+                int result = 1 + (1 / i);
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
