db8397ba3e0a8ddd39697234aeb089887dd18991
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 21:50:03 +0400

    123

---
+++ While_1/While_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = 8;
+            int N = 12;
+            int B = 0;
+            while (B < N)
+            {
+                Console.WriteLine(K);
+                B++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
