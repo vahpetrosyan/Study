7a0d2c58ce08dccefe1684455d2fa30b7d2dc2d9
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 23:06:47 +0400

    commit

---
+++ Write_6/Write_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 8;
+            int B = 14;
+            int n = A + 1;
+            while (n < B)
+            {
+                Console.WriteLine(n);
+                Console.WriteLine("-----------------");
+                n++;
+            }
+            int result = B - A;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
