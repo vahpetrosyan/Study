5203d266de45077681f84d6e8aab096914981ef8
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 23:52:35 +0400

    commit

---
+++ While_10/While_10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint N = Convert.ToUInt32(Console.ReadLine());
+            double result = 0;
+            int B = 0;
+            while (B <= N)
+            {
+                result += Math.Pow(B + N, 2);
+                B++;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
