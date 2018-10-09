5c459d71366ac3f4f20c8a0a38061c337e8ebdea
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 22:03:05 +0400

    commit

---
+++ While_3/While_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int degree = 1;
+            while (degree < 10)
+            {
+                double result = Math.Pow(N, degree);
+                Console.WriteLine(result);
+                degree++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
