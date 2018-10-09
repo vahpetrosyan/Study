88b95f8e23578efe6751fd20e924fdbcfd85a0c4
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 23:46:23 +0400

    commit

---
+++ While_9/While_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint A = Convert.ToUInt32(Console.ReadLine());
+            int N = 1;
+            double result = 0;
+            while (N <= A)
+            {
+             result += 1 / (double)N;
+             Console.WriteLine(result);
+             Console.WriteLine("----------------");
+             N++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
