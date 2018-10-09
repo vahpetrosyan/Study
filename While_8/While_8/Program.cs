494daf1ee0977686b1e5543071a62376ec4abb2f
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 23:19:02 +0400

    commit

---
+++ While_8/While_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 11;
+            int N = A + 1;
+            while (N < B)
+            {
+                int result = N % 3;
+                if(result == 0)
+                {
+                Console.WriteLine(N);
+                    break;
+                }
+                N++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
