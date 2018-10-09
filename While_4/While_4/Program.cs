a4d9ad8dac0574843409b8e070aa654e32fe19bc
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 22:09:03 +0400

    commit

---
+++ While_4/While_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 12;
+            int result = 0;
+            int B = 0;
+            while (B < N)
+            {
+                result += B;
+                B++;
+            }
+                Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
