9a94646b326c7781beb69d1355bfdc1d15cfd44d
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 23:14:28 +0400

    commit

---
+++ While_7/While_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 4;
+            int B = 9;
+            int sum = 0;
+            int n = A+1;
+            while (n < B)
+            {
+                sum += n;
+                n++;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
