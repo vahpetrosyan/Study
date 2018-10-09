fc4c2118c0ec31eb20d8332e6e67732d09cbbeb3
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-10 00:10:32 +0400

    commit

---
+++ While_11/While_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint N = Convert.ToUInt32(Console.ReadLine());
+            double result = 1;
+            int B = 0;
+            while (B <= N)
+            {
+                result *= 1 + (double)B / 10;
+                Console.WriteLine("-------------------");
+            Console.WriteLine(result);
+                B++;
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
