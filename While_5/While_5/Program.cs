79f8b27cb53e503c9f42281806e090864406f1f0
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-09 23:01:09 +0400

    commit

---
+++ While_5/While_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 12;
+            int result = 0;
+            int B = 0;
+            while (B < A)
+            {
+                result += B;
+                B += 2;
+            }
+                Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
