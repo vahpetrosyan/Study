b8295f496c6112282a4233c9b2b14ea675add982
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-11 13:44:51 +0400

    commit

---
+++ 16/16/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_է
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int j = 1; j < N; j++)
+            {
+                sum += j;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ խնդիր 16/խնդիր 16/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_16
+{
+    class Program
+    {
+
+        static void sum()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            int s = 0;
+            while (a > 0)
+            {
+
+                s += a % 10;
+                a = a / 10;
+
+            }
+            Console.WriteLine(s);
+        }
+
+
+        static void count()
+        {
+
+            int n = 52;
+          double b =  Math.Ceiling(Math.Log10(n));
+            Console.WriteLine(b);
+        }
+        static void multiplication()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            int s = 1;
+            while (a > 0)
+            {
+
+                s *= a % 10;
+                a = a / 10;
+
+            }
+            Console.WriteLine(s);
+        }
+
+
+        static void Main(string[] args)
+        {
+            sum();
+            count();
+            multiplication();
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
