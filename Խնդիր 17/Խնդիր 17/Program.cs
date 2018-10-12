26dfc2d171c51393e58944c07bbd858430b9f032
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-12 10:22:16 +0400

    commit

---
+++ Խնդիր 17/Խնդիր 17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace խնդիր_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(valueMinOddDigit());
+            Console.ReadKey();
+        }
+        static int valueMinOddDigit()
+        {
+          int num = Convert.ToInt32(Console.ReadLine());
+            int digit = 11;
+            while (num != 0)
+            {
+                int res = num % 10;
+                if (res%2!=0 && res < digit)
+                    digit = res;
+                num /= 10;
+            }
+            return digit;
+        }
+        
+        
+
+       
+    }
+}
