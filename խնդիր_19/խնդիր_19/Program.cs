59f512399d9fb289e0472bdf52df9dea7e1a674d
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-12 11:08:22 +0400

    commit

---
+++ խնդիր_19/խնդիր_19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_19
+{
+    class Program
+    {
+        static int evenNumberFunc()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int b = 1;
+            while (n != 0)
+            {
+                int s = n % 10;
+                if (even(s) == true && s < 7)
+                {
+                    b = b * s;
+                }
+                else
+                {
+                    Console.WriteLine("Your number doesn't have any even number");
+                }
+                n /= 10;
+            }
+            return b;
+        }
+        static bool even(int n)
+        {
+         return  n % 2 == 0;
+         
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(evenNumberFunc());
+            Console.ReadKey();
+        }
+    }
+}
