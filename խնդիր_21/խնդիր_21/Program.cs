20182155abd04f79f3f0bd370525db972889c44c
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-12 22:18:01 +0400

    commit

---
+++ խնդիր_21/խնդիր_21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            {
+
+                void findDivisors()
+                {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        int result = n % i;
+                        
+                        if (result == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                      
+                }
+                findDivisors();
+                Console.ReadKey();
+
+            }
+}   }   }
