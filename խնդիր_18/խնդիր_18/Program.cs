8454f0b2594ca7a4252c339020b8c432cfbcb7e9
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-12 10:52:57 +0400

    commit

---
+++ խնդիր_18/խնդիր_18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_18
+{
+    class Program
+    {
+        static int oddNumberFunc()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            while (n != 0)
+            {
+               int s = n % 10;
+                if(s%2 != 0 && s > 4)
+                {
+                    b =b+ s;
+                }
+                n /= 10;
+            }
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(oddNumberFunc());
+            Console.ReadKey();
+        }
+    }
+}
