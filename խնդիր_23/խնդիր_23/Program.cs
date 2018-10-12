dcc444d8b3bdb9cf3fc2b3dc562e54a0c7dbbb29
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-13 00:43:44 +0400

    final commit

--- խնդիր_23/խնդիր_23/Program.cs
+++ խնդիր_23/խնդիր_23/Program.cs
@@ -8,28 +8,30 @@ namespace խնդիր_23
 {
     class Program
     {
-        static void Main(string[] args)
+     static  void divisorsOperations()
         {
             int n = Convert.ToInt32(Console.ReadLine());
             int i;
             int sum = 1;
-                    int res = 0;
-                    int count = 0;
-            void divisorsOperations()
+            int res = 0;
+            int count = 0;
+            for (i = 1; i <= n; i++)
             {
-                for (i = 1; i <= n; i++)
+                int result = n % i;
+                if (result == 0)
                 {
-                    int result = n % i;
-                    if (result == 0)
-                    {
-                       res = (sum += i) - 1;
-                        count++;
-                        
-                    }
+                    res = (sum += i) - 1;
+                    count++;
+
                 }
-                    int finalResult = res / count;
-                    Console.WriteLine(finalResult);
             }
+            int finalResult = res / count;
+            Console.WriteLine(finalResult);
+        }
+        static void Main(string[] args)
+        {
+            
+
             divisorsOperations();
             Console.ReadKey();
         }
