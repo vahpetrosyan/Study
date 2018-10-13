cc7d1c1e96f5ab6097a9aa0e58671b855f224097
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-13 12:19:15 +0400

    commit

---
+++ խնդիր 26/խնդիր 26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_26
+{
+    class Program
+    {
+        static void degree()
+        {
+            double n = Convert.ToInt32(Console.ReadLine());
+            for (double i = 0; i < n; i++)
+            {
+                double result = Math.Pow(2, i);
+                if(result>n && Math.Pow(2, i - 1) <= n)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            degree();
+            Console.ReadKey();
+        }
+    }
+}
