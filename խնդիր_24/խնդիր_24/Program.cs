6c53784cc96e24df432111ccff9780f1313558ef
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-13 11:45:39 +0400

    commit

---
+++ խնդիր_24/խնդիր_24/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace խնդիր_24
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            valueDegree();
+            Console.ReadKey();
+        }
+        static void valueDegree()
+        {
+            double n = Convert.ToDouble(Console.ReadLine());
+            for (double i = 2; i < n; i++)
+            {
+                double result = Math.Pow(n, 1.0 / i);
+                if (result == 3)
+                {
+                    Console.WriteLine($"{n} is degree of 3");
+                    break;
+                }
+            }
+        }
+    }
+}
