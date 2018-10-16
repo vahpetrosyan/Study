d21b94a229f2bbb002a0c78a031de8265bd3c7a0
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-16 22:03:51 +0400

    commit

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(5));
+            Console.ReadLine();
+
+        }
+        static int factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            else
+                return n *= factorial(n - 1);
+
+        }
+    }
+}
