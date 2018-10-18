c181a43511dd54c070a8e1ff425c6abecda5486d
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-18 23:23:35 +0400

    commit

---
+++ Խնդիր_28/Խնդիր_28/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Խնդիր_28
+{
+    class Program
+    {
+        static void  digitOperate()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            string[] answer = new string[2] { "yes", "no" };
+            while (n>0)
+            {
+                result += n % 10;
+                n /= 10;
+            }
+            if (n == result * 2)
+            {
+                Console.WriteLine(answer[0]);
+            }
+            else
+            {
+                Console.WriteLine(answer[1]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            digitOperate();
+            Console.ReadKey();
+
+
+        }
+    }
+}
