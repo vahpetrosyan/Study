f230aaf0a62a564808731c89518b797384223997
Vahe Petrosyan <kitrun006@yandex.ru>
2018-10-22 00:59:30 +0400

    commit

---
+++ task_33/task_33/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_33
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            double N = 1;
+            double X = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                N *= x / i;
+                X += N;
+
+            }
+            Console.WriteLine(X);
+            Console.ReadKey();
+        }
+    }
+}
